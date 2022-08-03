a9025d5a30641ee55c862a032542e07d86c0f3df
Felipe Y Castro <felypeycastro@gmail.com>
2022-08-03 18:00:55 -0300

    adicionado documento html que recebe informações

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let enviar = document.querySelector("#enviar");
 
 enviar.addEventListener("click", function (e) {
-  e.preventDefault();
+  // e.preventDefault(); //esse evento eu quero enviar
   let name = document.querySelector("#name");
 
   let value = name.value;
